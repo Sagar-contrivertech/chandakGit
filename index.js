2a58438530fee3ab7f8d1a24cc4d6b4bf6734d23
Sagar-contrivertech <97974995+Sagar-contrivertech@users.noreply.github.com>
2022-05-19 15:48:40 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+
+const { google } = require('googleapis')
+
+const app = express()
+app.use(express.json())
+
+//authentications
+const authentication = async () => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "keys.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    });
+    const client = await auth.getClient();
+    const sheets = google.sheets({
+        version: 'v4',
+        auth: client
+    });
+    return { sheets }
+}
+
+const id = "13y0G3qCfkZ7zSM_c1djeBRQrZzZB_9qx_zz35Ke3aEU"// spreadsheet i
+// writing data toaspreadsheet
+app.post('/', async (req, res) => {
+    try {
+        // destructure'newName'and'newValue'from req.body
+        const { newName, newValue } = req.body;
+        const { sheets } = await authentication();
+        // writing data toaspreadsheet
+        const writeReq = await sheets.spreadsheets.values.append({
+            spreadsheetId: id,
+            range: 'Sheet1',
+            valueInputOption: 'USER_ENTERED',
+            resource: {
+                values: [
+                    [newName, newValue],
+                ]
+            }
+        })
+        if (writeReq.status === 200) {
+            return res.json({ msg: 'Spreadsheet updated successfully!' })
+        }
+        return res.json({ msg: 'Something went wrong while updating the spreadsheet.' })
+    } catch (e) {
+        console.log('ERROR UPDATING THE SPREADSHEET', e);
+        res.status(500).send();
+    }
+})
+app.listen(3000, () => console.log('server is running in 3000'))
