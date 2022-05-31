86070debcbca814df8f31ae508b85ff350b6f974
Sagar-contrivertech <97974995+Sagar-contrivertech@users.noreply.github.com>
2022-05-31 12:52:27 +0530

    Mail

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 
 const { google } = require('googleapis')
 
+const nodemailer = require('nodemailer')
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -35,7 +37,7 @@ app.post('/', async (req, res) => {
             valueInputOption: 'USER_ENTERED',
             resource: {
                 values: [
-                    [ Name, Number, Email ],
+                    [Name, Number, Email],
                 ]
             }
         })
@@ -48,4 +50,43 @@ app.post('/', async (req, res) => {
         res.status(500).send();
     }
 })
+
+
+app.post('/mail', async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "keval.hdpi@gmail.com",
+            pass: "keval@1234"
+        }
+    });
+
+    const mailOptions = {
+        from: req.body.Email,
+        to: 'keval.hdpi@gmail.com',
+        subject: ` New lead from ${req.body.Name} `,
+        text: `You have recievied new Lead from Website
+        Name : ${req.body.Name}
+        Number: ${req.body.Number}
+        Email: ${req.body.Email}
+        
+        Leads has also been update to sheet
+
+        https://docs.google.com/spreadsheets/d/13y0G3qCfkZ7zSM_c1djeBRQrZzZB_9qx_zz35Ke3aEU/edit?usp=sharing
+
+        `
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error)
+            res.status(400).send(error)
+        } else {
+            console.log('Email sent ' + info.response)
+            res.status(200).send('succes')
+        }
+    })
+})
+
+
 app.listen(3000, () => console.log('server is running in 3000'))
